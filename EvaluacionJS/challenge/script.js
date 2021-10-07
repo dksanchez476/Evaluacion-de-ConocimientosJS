314979cddcb1565f27a6b84e0d0302cd5720d291
dksanchez476 <83289024+dksanchez476@users.noreply.github.com>
2021-10-07 09:22:37 -0500

    Loading the evaluation JavaScript

---
+++ EvaluacionJS/challenge/script.js
@@ -0,0 +1,12 @@
+var socialMedia = {
+  facebook : 'http://facebook.com/viewsource',
+  twitter: 'http://twitter.com/planetoftheweb',
+  flickr: 'http://flickr.com/planetotheweb',
+  youtube: 'http://youtube.com/planetoftheweb',
+  wordpress: 'https://wordpress.com/'
+};
+lista = document.querySelectorAll('.socialmediaicons');
+document.write('<a href="http://flickr.com/planetotheweb"><img src="images/flickr.png"></a>');
+document.write('<a href="http://facebook.com/viewsource"><img src="images/facebook.png"></a>');
+document.write('<a href="http://twitter.com/planetoftheweb"><img src="images/twitter.png"></a>');
+document.write('<a href="http://youtube.com/planetofthewe"><img src="images/youtube.png"></a>');
